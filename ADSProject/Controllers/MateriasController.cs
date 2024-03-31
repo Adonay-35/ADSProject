c8b9eeb568503f43d5fc0ba4ba7dcb1b98916411
Adonay <sh21i04001@usonsonate.edu.sv>
2024-03-31 15:53:38 -0600

    Se agrega CRUD para Materias

---
+++ ADSProject/Controllers/MateriasController.cs
@@ -0,0 +1,142 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+using ADSProject.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADSProject.Controllers
+{
+        [Route("api/materias/")]
+        public class MateriasController : ControllerBase
+        {
+            private readonly IMateria materia;
+            public string pCodRespuesta;
+            private string pMensajeUsuario;
+            private string pMensajeTecnico;
+
+            public MateriasController(IMateria materia)
+            {
+                this.materia = materia;
+            }
+
+            [HttpPost("agregarMateria")]
+            public ActionResult<string> AgregarMateria([FromBody] Materia materia)
+            {
+                try
+                {
+                    int contador = this.materia.AgregarMateria(materia);
+
+                    if (contador > 0)
+                    {
+                        pCodRespuesta = Constans.COD_ERROR;
+                        pMensajeUsuario = "Registro insertado con exito";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                    }
+                    else
+                    {
+                        pCodRespuesta = Constans.COD_ERROR;
+                        pMensajeUsuario = "Ocurrio un problema al insertar el registro";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                    }
+                    return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            [HttpPut("actualizarMateria/{idMateria}")]
+            public ActionResult<string> ActualizarMateria(int idMateria, [FromBody] Materia materia)
+            {
+                try
+                {
+                    int contador = this.materia.ActualizarMateria(idMateria, materia);
+                    if (contador > 0)
+                    {
+                        pCodRespuesta = Constans.COD_EXITO;
+                        pMensajeUsuario = "Registro actualizado con exito";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                    }
+                    else
+                    {
+                        pCodRespuesta = Constans.COD_ERROR;
+                        pMensajeUsuario = "Ocurrio un problema al actualizar el registro";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                    }
+                    return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            [HttpDelete("eliminarMateria/{idMateria}")]
+            public ActionResult<string> EliminarMateria(int idMateria)
+            {
+                try
+                {
+                    bool eliminado = this.materia.EliminarMateria(idMateria);
+
+                    if (eliminado)
+                    {
+                        pCodRespuesta = Constans.COD_EXITO;
+                        pMensajeUsuario = "Registro eliminado con exito";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                    }
+                    else
+                    {
+                        pCodRespuesta = Constans.COD_ERROR;
+                        pMensajeUsuario = "Ocurrio un problema al eliminar el registro";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                    }
+                    return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            [HttpGet("obtenerMateriaPorID/{idMateria}")]
+            public ActionResult<Materia> ObtenerMateriaPorID(int idMateria)
+            {
+                try
+                {
+                    Materia materia = this.materia.ObtenerMateriaPorID(idMateria);
+                    if (materia != null)
+                    {
+                        return Ok(materia);
+                    }
+                    else
+                    {
+                        pCodRespuesta = Constans.COD_ERROR;
+                        pMensajeUsuario = "No se encontraron datos del estudiante";
+                        pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+
+                        return NotFound(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            [HttpGet("obtenerMaterias")]
+            public ActionResult<List<Materia>> ObtenerMaterias()
+            {
+                try
+                {
+                    List<Materia> lstMaterias = this.materia.ObtenerTodosLasMaterias();
+
+                    return Ok(lstMaterias);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
---
+++ ADSProject/Repositories/MateriaRepository.cs
@@ -0,0 +1,90 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+
+namespace ADSProject.Repositories
+{
+    public class MateriaRepository : IMateria
+    {
+        private List<Materia> lstMaterias = new List<Materia>
+        {
+            new Materia{ Id = 1, Nombre = "Desarrollo de Software"
+            }
+        };
+
+        public int ActualizarMateria(int idMateria, Materia Materia)
+        {
+            try
+            {
+                int indice = lstMaterias.FindIndex(tmp => tmp.Id == idMateria);
+
+                lstMaterias[indice] = Materia;
+
+                return idMateria;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int AgregarMateria(Materia materia)
+        {
+            try
+            {
+                if (lstMaterias.Count > 0)
+                {
+                    materia.Id = lstMaterias.Last().Id + 1;
+                }
+
+                lstMaterias.Add(materia);
+
+                return materia.Id;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool EliminarMateria(int idMateria)
+        {
+            try
+            {
+                int indice = lstMaterias.FindIndex(tmp => tmp.Id == idMateria);
+
+                lstMaterias.RemoveAt(indice);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Materia ObtenerMateriaPorID(int idMateria)
+        {
+            try
+            {
+                Materia materia = lstMaterias.FirstOrDefault(tmp => tmp.Id == idMateria);
+                return materia;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Materia> ObtenerTodosLasMaterias()
+        {
+            try
+            {
+                return lstMaterias;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+    }
